8778a54c6298a105ae0bfea812b39783fab02e34
Ismail <69149337+ismail-x@users.noreply.github.com>
2024-11-01 22:19:33 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/KiosLink/', // Set the base path for GitHub Pages deployment
+  base: 'https://ismail-x.github.io/KiosLink/', // Set the base path for GitHub Pages deployment
 })
